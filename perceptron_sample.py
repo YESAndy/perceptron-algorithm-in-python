9a4fedf9816a95276573d0a44ab9d5ce6407cb2e
Cai <35024963+YESAndy@users.noreply.github.com>
2018-10-05 23:46:12 +0800

    Update perceptron_sample.py

--- perceptron_sample.py
+++ perceptron_sample.py
@@ -2,91 +2,98 @@
 import numpy as np
 import random
 
-class perceptron:
-    
 
+def train(max_itera):
     
-    def train(self):
+    
+    learning_rate=0.1
+    num_instances=100
+    theta=0
+    weights=np.zeros(4)
+    local_err=0
+    global_err=0
+    output=0
+    outputs=np.zeros(num_instances)
+    iteration=0
+    #create train points
+    
+    x=np.zeros(num_instances)
+    y=np.zeros(num_instances)
+    z=np.zeros(num_instances)
+    #class1
+    for i in range(int(num_instances/2)):
+        x[i]=random.randrange(4,11)
+        y[i]=random.randrange(5,8)
+        z[i]=random.randrange(2,6)
+    #class2   
+    for i in range(int(num_instances/2),num_instances):
+        x[i]=random.randrange(-2,3)
+        y[i]=random.randrange(-4,5)
+        z[i]=random.randrange(-6,2)
+     
+    for i in range(4):
+        weights[i]=random.randrange(0,1)
         
-        self.max_itera=100
-        self.learning_rate=1
-        self.num_instances=100
-        self.theta=0
-        self.weights=np.zeros(4)
-        self.local_err=0
-        self.global_err=0
-        self.output=0
-        self.outputs=np.zeros(self.num_instances)
-        iteration=0
-        #create train points
+    
+    output=calculate_output(theta,weights,x[0],y[0],z[0])
+    local_err=outputs[0]-output
+    
+    weights[0]+=learning_rate*local_err*x[0]
+    weights[1]+=learning_rate*local_err*y[0]
+    weights[2]+=learning_rate*local_err*z[0]
+    weights[3]+=learning_rate*local_err
+
+    global_err+=local_err*local_err
+    
+    while(global_err!=0 and iteration<=max_itera):    
+        iteration+=1
         
-        x=np.zeros(self.num_instances)
-        y=np.zeros(self.num_instances)
-        z=np.zeros(self.num_instances)
-        #class1
-        for i in range(self.num_instances/2):
-            x[i]=random.randrange(4,11)
-            y[i]=random.randrange(5,8)
-            z[i]=random.randrange(2,6)
-        #class2   
-        for i in range(self.num_instances/2,self.num_instances):
-            x[i]=random.randrange(-2,3)
-            y[i]=random.randrange(-4,5)
-            z[i]=random.randrange(-6,2)
-         
-        for i in range(4):
-            self.weights[i]=random.randrange(0,1)
+        for i in range(1,num_instances):
             
-        while(self.global_err!=0 and iteration<=self.max_itera):
+            output=calculate_output(theta,weights,x[i],y[i],z[i])
+            local_err=outputs[i]-output
             
-            iteration+=1
-            
-            for i in range(self.num_instances):
-                
-                self.output=self.calculate_output(self.theta,self.weights,x[i],y[i],z[i])
-                self.local_err=self.outputs[i]-self.output
-                
-                self.weights[0]+=self.learning_rate*self.local_err*x[i]
-                self.weights[1]+=self.learning_rate*self.local_err*y[i]
-                self.weights[2]+=self.learning_rate*self.local_err*z[i]
-                self.weights[3]+=self.learning_rate*self.local_err
-            
-                self.global_err+=self.local_err*self.local_err
-            
-            print("Iteration "+iteration+": RMSE= "+np.sqrt(self.global_err/self.num_instances))
+            weights[0]+=learning_rate*local_err*x[i]
+            weights[1]+=learning_rate*local_err*y[i]
+            weights[2]+=learning_rate*local_err*z[i]
+            weights[3]+=learning_rate*local_err
         
-        test_output=self.test(self.theta,self.weights)
+            global_err+=local_err*local_err
+        
+        print("Iteration "+str(iteration)+": RMSE= "+str(np.sqrt(global_err/num_instances)))
+       
+    #test data
+    x_test=np.zeros(10)
+    y_test=np.zeros(10)
+    z_test=np.zeros(10)
+    
+    for i in range(10):
+        x_test[i]=random.randrange(-10,11)
+        y_test[i]=random.randrange(-10,11)
+        z_test[i]=random.randrange(-10,11)
+
+        test_output=calculate_output(theta,weights,x_test[i],y_test[i],z_test[i])
+        
         print("\n=======\nNew Random Point:")
-
-        print("class = "+test_output)
-        
-        
-    def test(self,theta,weights):
-        
-        x_test=np.zeros(10)
-        y_test=np.zeros(10)
-        z_test=np.zeros(10)
-        
-        for i in range(10):
-            x_test[i]=random.randrange(-10,11)
-            y_test[i]=random.randrange(-10,11)
-            z_test[i]=random.randrange(-10,11)
+        print("x = "+str(x_test[i])+",y = "+str(y_test[i])+ ",z = "+str(z_test[i]))
+        print("class = "+str(test_output))
     
-        output=self.calculate_output(theta,weights,x_test,y_test,z_test)
-        
-        return output
-
-        
-    def calculate_output(theta,weights,x,y,z):
-        
-        summary=x*weights[0]+y*weights[1]+z*weights[2]
-        
-        if summary>= theta:
-            return 1
-        else: return 0
+def calculate_output(theta,weights,x,y,z):
+    
+    summary=x*weights[0]+y*weights[1]+z*weights[2]+weights[3]
+    
+    if summary>= theta:
+        return 1
+    else: return 2
         
 if __name__=="__main__":
     
-    percept=perceptron
+    train_percept=train
     
-    percept.train
+    train_percept(100)
+    
+    
+        
+    
+    
+    
